70d7f23f46775329d65e2dfc2938bd5ecab7efbb
Mohammad Amir <amir@connextar.com>
2024-09-19 13:50:31 +0100

    Trying to get cas and jina working on Windows/Python 3.12, but no success so far.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from lib.client import encode as cas_encode
 # ---------- Main script
 if __name__ == '__main__':
     # ---------- Run clip demo
-    # clip_demo()
+    clip_demo()
 
     # ---------- Run CAS encode demo
-    cas_encode()
+    # cas_encode()
